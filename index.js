10981de2246d3e004069a49187cf53cd2bfa6edc
Pedro A. Ventura <pedro-alexandre-silva@telecom.pt>
2022-07-15 18:37:38 +0100

    Everything

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoClient = require('mongodb').MongoClient;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/api/test', (req, res) => {
+    
+    mongoClient.connect('mongodb://localhost:27017', (err, client) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('Connected to MongoDB');
+
+        const db = client.db('sample_training');
+        
+        const collection = db.collection('test');
+        
+        collection.find().toArray((err, docs) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log(docs);
+            res.send(docs);
+        });
+    });
+
+});
+
+app.post('/api/test', (req, res) => {
+    const data = req.body;
+
+    mongoClient.connect('mongodb://localhost:27017', (err, client) => {
+
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('Connected to MongoDB');
+
+        const db = client.db('sample_training');
+
+        const collection = db.collection('test');
+
+        collection.insertOne(data, (err, result) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log(result);
+            res.send(result);
+        });
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
