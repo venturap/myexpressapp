869e7ba1de25fcaff488aaba6b5a79f26f071439
Pedro A. Ventura <pedro-alexandre-silva@telecom.pt>
2022-07-15 21:37:37 +0100

    WIP

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const app = express();
 const port = 3000;
 const mongoClient = require('mongodb').MongoClient;
 
+app.use((req, res, next) => {
+        console.log('request received path: ' + req.path);
+        next();
+    }
+);
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+// READ
 app.get('/api/test', (req, res) => {
     
     mongoClient.connect('mongodb://localhost:27017', (err, client) => {
@@ -35,7 +42,7 @@ app.get('/api/test', (req, res) => {
     });
 
 });
-
+// CREATE
 app.post('/api/test', (req, res) => {
     const data = req.body;
 
@@ -61,6 +68,88 @@ app.post('/api/test', (req, res) => {
         });
     });
 });
+
+// LIST SPECIFIC DOCUMENT
+app.get('/api/test/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    mongoClient.connect('mongodb://localhost:27017', (err, client) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+            return;
+        }
+        console.log('Connected to MongoDB');
+        const db = client.db('sample_training');
+        const collection = db.collection('test');
+        collection.findOne({_id: id}, (err, doc) => {
+            if (err) {
+                console.log(err);
+                res.sendStatus(500);
+                return;
+            }
+            console.log(doc);
+            res.json(doc);
+        });
+    });
+});
+
+// UPDATE
+app.patch('/api/test/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const data = req.body;
+
+    mongoClient.connect('mongodb://localhost:27017', (err, client) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+            return;
+        }
+        console.log('Connected to MongoDB');
+        const db = client.db('sample_training');
+        const collection = db.collection('test');
+        collection.updateOne({_id: id}, {$set: data}, (err, result) => {
+            if (err) {
+                console.log(err);
+                res.sendStatus(500);
+                return;
+            }
+            console.log(result);
+            res.json(result);
+        });
+    });
+});
+
+// DELETE
+app.delete('/api/test/:id', (req, res) => {
+
+    const id = parseInt(req.params.id);
+
+    mongoClient.connect('mongodb://localhost:27017', (err, client) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+            return;
+        }
+        console.log('Connected to MongoDB');
+        const db = client.db('sample_training');
+        const collection = db.collection('test');
+        collection.deleteOne({_id: id}, (err, result) => {
+            if (err) {
+                console.log(err);
+                res.sendStatus(500);
+                return;
+            }
+            console.log(result);
+            if (result.deletedCount === 0) {
+                res.status(404).json({message: 'No document found'});
+                return;
+            }
+            res.json(result);
+        });
+    });
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
